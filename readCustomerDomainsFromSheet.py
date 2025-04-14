4c2186c4ef731c35abc6065c4c752ca4725c7b6f
Joseph Heinz <jbheinz14@gmail.com>
2025-04-14 15:53:50 -0500

    couple of debugging statements

--- readCustomerDomainsFromSheet.py
+++ readCustomerDomainsFromSheet.py
@@ -6,6 +6,9 @@ def read_customer_domains(csv_filename, id_file_header="id", search_domain_file_
     with open(csv_filename, mode="r", encoding="utf-8") as file:
         reader = csv.DictReader(file)  # Reads CSV into a dictionary format
         for row in reader:
+            print(f"Row: {row}")
+            print(f"Row ID: {row[id_file_header]}")
+            print(f"Row Domain: {row[search_domain_file_header]}")
             domain_from_customer_id[row[id_file_header]] = row[search_domain_file_header]
             
     return domain_from_customer_id
